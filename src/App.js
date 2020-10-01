24419b80bb74fddab8f20f29f86cba5c2c784b67
Subhankhalid1 <rsubhakhalid@gmail.com>
2020-10-02 00:44:31 +0500

    Mocking Server(using Miragejs)
    In this file we create own our server using miragejs. Which we use to create a server using mirage and render a data using api call (fetch api) or upadate the server nd UI/table for using Post method given by a server.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+
+    setInterval(() => {
+
+      fetch("/api/books")
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setBooks(data);
+
+        })
+
+    }, 2000);
+
+  }, [])
+
+  if (!books.length)
+    return <h2>Loading..</h2>
+
+
+  const addBook = () => {
+    const title = prompt("Enter Book Title");
+    const author = prompt("Enter Book Author");
+    const price = prompt("Enter Book Price");
+    const releasedDate = prompt("Enter Book Released Date");
+
+    console.log(title, author, price, releasedDate);
+    if (!title || !author || !price || !releasedDate)
+      return false;
+
+    fetch("/api/add", {
+      method: "POST",
+      body: JSON.stringify({ title, author, price, releasedDate })
+    }).
+      catch((error) => {
+
+        console.log("Error" + error);
+
+      })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 >Avaiable Books </h2>
+      <table border="6">
+        <th>Title</th>
+        <th>Author-Name</th>
+        <th>Price</th>
+        <th>Released-Date</th>
+        <tbody>
+          {books.map((booksObj, ind) => {
+            return (
+              <tr key={ind}>
+                <td>{booksObj.title}</td>
+                <td>{booksObj.author}</td>
+                <td>{booksObj.price}</td>
+                <td>{booksObj.releasedDate}</td>
+
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <button onClick={addBook}>Add Book</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {makeServer} from './server.js'
+makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <App />
